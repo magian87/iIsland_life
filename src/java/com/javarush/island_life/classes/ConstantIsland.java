acb5592e2e7b2ea25e65ed2be7de78961bdee5bd
magian87 <valiakhmetovbz@gmail.com>
2022-06-15 18:51:14 +0500

    Add files

---
+++ src/java/com/javarush/island_life/classes/ConstantIsland.java
@@ -0,0 +1,8 @@
+package com.javarush.island_life.classes;
+
+public class ConstantIsland
+{
+    public static String STEP_INFO = "Ходит: %s с клетки: %d\\%d на клетку: %d\\%d  ;";
+    public static String STEP_INFO_CHANGE_DIRECTION_IN_WATHER =
+            "Ходит: %s с клетки: %d\\%d дальше идти не куда, смена направления движения на %s ;";
+}
